a145411d160a696e3f51e0e0ca77ad35b7b624be
mpmartinez <40714049+mpmartinez@users.noreply.github.com>
2026-03-10 16:54:13 +0800

    fix: add Employee navigation property to LeaveBalance

--- src/PeopleCore.Domain/Entities/Leave/LeaveBalance.cs
+++ src/PeopleCore.Domain/Entities/Leave/LeaveBalance.cs
@@ -1,8 +1,11 @@
+using PeopleCore.Domain.Entities.Employees;
+
 namespace PeopleCore.Domain.Entities.Leave;
 
 public class LeaveBalance : AuditableEntity
 {
     public Guid EmployeeId { get; set; }
+    public Employee Employee { get; set; } = null!;
     public LeaveType LeaveType { get; set; } = null!;
     public Guid LeaveTypeId { get; set; }
     public int Year { get; set; }
